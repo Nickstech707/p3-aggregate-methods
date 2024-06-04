3ed9b95a34d8c2b8207a6a146a72543befdb1949
Nicholas Muriithi <nickstech707@gmail.com>
2024-06-04 23:28:29 +0300

    adds aggregate methods to collect data about objects using their related objects

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
@@ -15,9 +16,24 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        return len(self._enrollments)
+
+    def get_enrolled_courses(self):
+        courses = []
+        for enrollment in self._enrollments:
+            courses.append(enrollment.course)
+        return courses
+
+    def average_grade(self):
+        total_grades = sum(enrollment.grade for enrollment in self._enrollments)
+        num_courses = len(self._enrollments)
+        average_grade = total_grades / num_courses
+        return average_grade
+
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -29,6 +45,17 @@ class Course:
 
     def get_enrollments(self):
         return self._enrollments.copy()
+
+    def student_count(self):
+        return len(self._enrollments)
+
+    @classmethod
+    def enrollment_count_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
 
 
 class Enrollment:
@@ -45,3 +72,11 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    def average_grade_per_course(self):
+        grades_per_course = {}
+        for enrollment in self.student.get_enrollments():
+            course = enrollment.course
+            grades_per_course[course] = grades_per_course.get(course, []) + [enrollment.grade]
+        average_grades_per_course = {course: sum(grades) / len(grades) for course, grades in grades_per_course.items()}
+        return average_grades_per_course
